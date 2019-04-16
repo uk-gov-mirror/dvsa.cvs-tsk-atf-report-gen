fb7788de05db121a64e52836c5b91d0e5de2a7c9
Matt Clarke <mdclarke@deloitte.co.uk>
2019-04-16 15:50:33 +0100

    CVSB-4102

--- src/services/ReportGenerationService.ts
+++ src/services/ReportGenerationService.ts
@@ -3,8 +3,8 @@ import * as Excel from "exceljs";
 import * as path from "path";
 import {Service} from "../models/injector/ServiceDecorator";
 import {TestResultsService} from "./TestResultsService";
-import moment = require("moment");
-import {ActivityType} from "../models/enums";
+import moment = require("moment-timezone");
+import {ActivityType, TimeZone} from "../models/enums";
 
 @Service()
 class ReportGenerationService {
@@ -35,12 +35,12 @@ class ReportGenerationService {
                 siteVisitDetails.assesor.value = activity.testerName;
                 siteVisitDetails.siteName.value = activity.testStationName;
                 siteVisitDetails.siteNumber.value = activity.testStationPNumber;
-                siteVisitDetails.date.value = moment(activity.startTime).format("DD/MM/YYYY");
-                siteVisitDetails.startTime.value = moment(activity.startTime).format("HH:mm:ss");
+                siteVisitDetails.date.value = moment(activity.startTime).tz(TimeZone.LONDON).format("DD/MM/YYYY");
+                siteVisitDetails.startTime.value = moment(activity.startTime).tz(TimeZone.LONDON).format("HH:mm:ss");
 
                 // Populate declaration
-                declaration.date.value = moment(activity.endTime).format("DD/MM/YYYY");
-                declaration.finishTime.value = moment(activity.endTime).format("HH:mm:ss");
+                declaration.date.value = moment(activity.endTime).tz(TimeZone.LONDON).format("DD/MM/YYYY");
+                declaration.finishTime.value = moment(activity.endTime).tz(TimeZone.LONDON).format("HH:mm:ss");
 
                 // Populate activity report
                 for (let i = 0, j = 0; i < template.reportTemplate.activityDetails.length && j < testResults.length; i++, j++) {
@@ -49,20 +49,20 @@ class ReportGenerationService {
                     const testType: any = testResult.testTypes;
 
                     detailsTemplate.activity.value = (activity.activityType === "visit") ? ActivityType.TEST : ActivityType.WAIT_TIME;
-                    detailsTemplate.startTime.value = moment(testResult.testStartTimestamp).format("HH:mm:ss");
-                    detailsTemplate.finishTime.value = moment(testResult.testEndTimestamp).format("HH:mm:ss");
+                    detailsTemplate.startTime.value = moment(testResult.testStartTimestamp).tz(TimeZone.LONDON).format("HH:mm:ss");
+                    detailsTemplate.finishTime.value = moment(testResult.testEndTimestamp).tz(TimeZone.LONDON).format("HH:mm:ss");
                     detailsTemplate.vrm.value = testResult.vrm;
                     detailsTemplate.testDescription.value = testType.testTypeName;
                     detailsTemplate.seatsAndAxles.value = (testResult.vehicleType === "psv") ? testResult.numberOfSeats : "" ;
                     detailsTemplate.result.value = testType.testResult;
                     detailsTemplate.certificateNumber.value = testType.certificateNumber;
-                    detailsTemplate.expiryDate.value = moment(testType.testExpiryDate).format("DD/MM/YYYY");
+                    detailsTemplate.expiryDate.value = moment(testType.testExpiryDate).tz(TimeZone.LONDON).format("DD/MM/YYYY");
                 }
 
                 return template.workbook.xlsx.writeBuffer()
                 .then((buffer: Excel.Buffer) => {
                     return {
-                        fileName: `ATFReport_${moment(activity.startTime).format("DD-MM-YYYY")}_${moment(activity.startTime).format("HHmm")}_${activity.testStationPNumber}_${activity.testerName}.xlsx`,
+                        fileName: `ATFReport_${moment(activity.startTime).tz(TimeZone.LONDON).format("DD-MM-YYYY")}_${moment(activity.startTime).tz(TimeZone.LONDON).format("HHmm")}_${activity.testStationPNumber}_${activity.testerName}.xlsx`,
                         fileBuffer: buffer
                     };
                 });
