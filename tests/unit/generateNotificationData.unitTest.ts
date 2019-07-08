bc17e9c9794c1e6bca71ed21c2938ca4291474b5
khodder <khodder@gmail.com>
2019-07-08 09:39:45 +0100

    CVSB-6377 - fix linting

--- tests/unit/generateNotificationData.unitTest.ts
+++ tests/unit/generateNotificationData.unitTest.ts
@@ -12,7 +12,7 @@ describe("notificationData", () => {
         const event: any = JSON.parse(fs.readFileSync(path.resolve(__dirname, "../resources/queue-event.json"), "utf8"));
         const visit: any = JSON.parse(event.Records[0].body);
         const testResultsList: any = JSON.parse(fs.readFileSync(path.resolve(__dirname, "../resources/test-results-200-response.json"), "utf8"));
-        const testResultsList_MultiTest: any = JSON.parse(fs.readFileSync(path.resolve(__dirname, "../resources/test-results-200-response-multi-test.json"), "utf8"));
+        const testResultsListMultiTest: any = JSON.parse(fs.readFileSync(path.resolve(__dirname, "../resources/test-results-200-response-multi-test.json"), "utf8"));
         LambdaMockService.populateFunctions();
         context("when parsing the visit and the test results", () => {
             it("should return a correct test stations emails", () => {
@@ -30,7 +30,7 @@ describe("notificationData", () => {
 
         context("when parsing the visit and multiple test results", () => {
             it("should return a correct test stations emails with dividers", () => {
-                const testResultsArray = JSON.parse(testResultsList_MultiTest.body);
+                const testResultsArray = JSON.parse(testResultsListMultiTest.body);
                 const sendNotificationData = notificationData.generateActivityDetails(visit, testResultsArray);
                 expect(sendNotificationData.testStationPNumber).to.equal(testResultsArray[0].testStationPNumber);
                 expect(sendNotificationData.testerName).to.equal(visit.testerName);
@@ -39,7 +39,7 @@ describe("notificationData", () => {
                 expect(sendNotificationData.endTime).to.equal("15:36:33");
                 expect(sendNotificationData.activityType).to.equal("Test");
                 expect(sendNotificationData.activityDetails.length).to.not.equal(0);
-                expect(countInstances(sendNotificationData.activityDetails,'---')).to.equal(2);
+                expect(countInstances(sendNotificationData.activityDetails,"---")).to.equal(2);
             });
         });
     });
@@ -54,16 +54,17 @@ describe("notificationData", () => {
     });
 });
 
-let countInstances = (str: string, searchStr: string) => {
-    var searchStrLen = searchStr.length;
-    if (searchStrLen == 0) {
+const countInstances = (str: string, searchStr: string) => {
+    const searchStrLen = searchStr.length;
+    if (searchStrLen === 0) {
         return [];
     }
-    var startIndex = 0, index, indices = [];
-    console.log('XXXX', str)
+    let startIndex = 0;
+    let index;
+    const indices = [];
     while ((index = str.indexOf(searchStr, startIndex)) > -1) {
         indices.push(index);
         startIndex = index + searchStrLen;
     }
     return indices.length;
-}
+};
