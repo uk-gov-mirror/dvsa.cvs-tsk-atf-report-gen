a0e0a8ecdb11dbcc1016157b1903a88425402a65
khodder <khodder@gmail.com>
2019-11-01 17:32:35 +0000

    CVSB-6660 extend test coverage

---
+++ tests/unit/ActivitiesService.unitTest.ts
@@ -0,0 +1,47 @@
+import { ActivitiesService } from "../../src/services/ActivitiesService";
+import { wrapLambdaResponse } from "../util/responseUtils";
+import waitResponse from "../resources/wait-time-response.json";
+import { LambdaService } from "../../src/services/LambdaService";
+
+describe("Activities Service", () => {
+  describe("getActivities function", () => {
+    context("Lambda client returns a single record in expected format", () => {
+      it("returns parsed result", async () => {
+        const mockLambdaService = jest.fn().mockImplementation(() => {
+          return {
+            invoke: () =>Promise.resolve(wrapLambdaResponse(JSON.stringify(waitResponse))),
+            validateInvocationResponse: LambdaService.prototype.validateInvocationResponse
+          };
+        });
+        const activityService= new ActivitiesService(new mockLambdaService());
+        const result = await activityService.getActivities({});
+        expect(result).toEqual([{
+          startTime: "2019-01-14T10:42:33.987Z",
+          endTime: "2019-01-14T10:48:33.987Z",
+          waitReason: "Break"
+        }]);
+      });
+    });
+
+    context("Lambda client returns multiple records in expected format", () => {
+      it("returns sorted result set", async () => {
+        const waitActivity = JSON.parse(waitResponse.body)[0];
+        const latestActivity = {...waitActivity, startTime: "2019-01-14T10:42:33.987Z"};
+        const middleActivity = {...waitActivity, startTime: "2019-01-14T09:42:33.987Z"};
+        const earliestActivity = {...waitActivity, startTime: "2019-01-14T08:42:33.987Z"};
+        const myEvent = {...waitResponse, body: JSON.stringify([latestActivity, earliestActivity, middleActivity, middleActivity])};
+
+        const mockLambdaService = jest.fn().mockImplementation(() => {
+          return {
+            invoke: () =>Promise.resolve(wrapLambdaResponse(JSON.stringify(myEvent))),
+            validateInvocationResponse: LambdaService.prototype.validateInvocationResponse
+
+          };
+        });
+        const activityService = new ActivitiesService(new mockLambdaService());
+        const result = await activityService.getActivities({});
+        expect(result).toStrictEqual([earliestActivity, middleActivity, middleActivity, latestActivity]);
+      });
+    });
+  });
+});
